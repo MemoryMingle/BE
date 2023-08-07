8518842399618ebdb2c2fa2e6dae32e5080a8984
mjmcode <mjm7542>
2023-08-08 04:31:02 +0900

    feat : Memories POST API  기능 구현

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const signupRouter = require("./signup.route");
 const loginRouter = require("./login.route");
 const groupRouter = require("./group.route");
+const memoryRouter = require("./memory.route")
 
 router.get("/", (req, res) => {
   res.send("이게 왜 안됨?");
@@ -11,5 +12,7 @@ router.get("/", (req, res) => {
 router.use("/signup", signupRouter);
 router.use("/login", loginRouter);
 router.use("/group", groupRouter);
+groupRouter.use("/:groupId/memory", memoryRouter);
+
 
 module.exports = router;
