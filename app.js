92a100da15da110d4babd61bdf554afbe28bd1ee
mjmcode <125952973+mjm7542@users.noreply.github.com>
2023-08-07 21:27:30 +0900

    fix : origin: 'http://localhost:3000' (#10)
    Co-authored-by: mjmcode <mjm7542>

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: 'http://localhost:*',
+        origin: 'http://localhost:3000',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
         credentials: true,
     })
