5ba97e0389ce3dcd1277353aa5d081fa7b5cdf40
mjmcode <mjm7542>
2023-08-08 12:50:43 +0900

    fix : 회원가입 nickname 미입력 기본값 "닉네임" 설정

--- src/services/signup.service.js
+++ src/services/signup.service.js
@@ -5,11 +5,11 @@ const SignupRepository = require('../repositories/signup.repository');
 class SignupService {
     signupRepository = new SignupRepository();
 
-    signup = async (loginId, nickname, password) => {
+    signup = async (loginId, password, nickname) => {
 
         // const salt = await bcrypt.genSalt(10);
         // const hashedPassword = await bcrypt.hash(password, salt);
-        await this.signupRepository.signup(loginId, nickname, password);
+        await this.signupRepository.signup(loginId, password, nickname);
     };
 }
 
