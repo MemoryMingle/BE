6bf2fe80fd22fe8ffe849dc6a7c41f290ad134f6
mjmcode <mjm7542>
2023-08-11 17:42:15 +0900

    fix : 세션 다시 설정

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -7,28 +7,35 @@ router.post('/', passport.authenticate('local', { session: false }), (req, res) 
     // 위에서 done이 req.user로 반환된다.
     res.cookie("MM", `Bearer ${req.user.accessToken}`, {
         secure: true,
-        httpOnly: false,
+        httpOnly: true,
         sameSite: "none",
     });
     res.status(200).json({ message: "로그인 완료" });
 });
 
 // 카카오 로그인
-router.get("/kakao", passport.authenticate("kakao"));
+router.get("/kakao", passport.authenticate("kakao", { session: false }));
 router.get(
     "/kakao/callback", // 카카오 인증이 성공하면 이 API실행됨
     passport.authenticate("kakao", { failureRedirect: "/" }),
     // 로그인 인증 실패 시 이동할 주소
     (req, res) => {
         const user = req.user;
-        // JWT 생성
-        const accessToken = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, {
-            expiresIn: "1h",
-        });
-        // JWT를 쿠키에 담아 클라이언트에게 전달
-        res.cookie("MM", `Bearer ${accessToken}`);
-        res.redirect("http://localhost:3000/api/login");
-        res.status(200).json({ message: "로그인 완료" });
+        try {
+            // JWT 생성
+            const accessToken = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, {
+                expiresIn: "1h",
+            });
+            // JWT를 쿠키에 담아 클라이언트에게 전달
+            res.cookie("MM", `Bearer ${req.user.accessToken}`, {
+                secure: true,
+                httpOnly: true,
+                sameSite: "none",
+            });
+            res.status(200).json({ message: "로그인 완료" });
+        } catch (error) {
+            res.status(500).json({ message: "요청을 처리하는 동안 오류가 발생했습니다." });
+        }
     }
 );
 
