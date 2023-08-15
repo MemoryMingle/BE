64ba24c17352bac799876079b75fb8cf6d0d8ef8
Obligedwalnut <benerogic@gmail.com>
2023-08-15 14:39:48 +0900

    feat: userInfo api 구현

--- src/middlewares/asyncHandler.js
+++ src/middlewares/asyncHandler.js
@@ -1,12 +1,11 @@
 const asyncHandler = (requestHandler) => {
-    return async (req, res, next) => {
-        try {
-            await requestHandler(req, res);
-        } catch (error) {
-            next(error);
-        }
+  return async (req, res, next) => {
+    try {
+      await requestHandler(req, res, next);
+    } catch (error) {
+      next(error);
     }
+  };
 };
 
-
-module.exports = asyncHandler
+module.exports = asyncHandler;
