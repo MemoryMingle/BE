8d4a7751da3779a75aea5035f332a27bf7ee168a
mjmcode <mjm7542>
2023-08-16 10:38:17 +0900

    fix : 변수명 오타 수정

--- src/routes/auth/signup.route.js
+++ src/routes/auth/signup.route.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const asyncHandler = require('../../middlewares/asyncHandler')
-const SiginupController = require("../../controllers/signup.controller");
-const siginupController = new SiginupController();
+const SignupController = require("../../controllers/signup.controller");
+const signupController = new SignupController();
 
 const router = express.Router();
 
-router.post("/", asyncHandler(siginupController.signup));
-router.post("/check", asyncHandler(siginupController.checkDuplicate));
-router.put("/update", asyncHandler(siginupController.updateProfile));
+router.post("/", asyncHandler(signupController.signup));
+router.post("/check", asyncHandler(signupController.checkDuplicate));
+router.put("/update", asyncHandler(signupController.updateProfile));
 
 
 module.exports = router;
