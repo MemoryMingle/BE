0194563f3091882af43ba7e1fdd030b291a91dd6
Obligedwalnut <benerogic@gmail.com>
2023-08-22 17:51:45 +0900

    feat : group 상세보기 userId 추가

--- src/services/group.service.js
+++ src/services/group.service.js
@@ -111,6 +111,7 @@ class GroupService {
     );
 
     const detailedGroupData = {
+      userId:groupData.userId,
       groupId: groupData.groupId,
       groupName: groupData.groupName,
       place: groupData.place,
