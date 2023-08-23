be3be5b46b5bf5a4631ef8548d41a2ca34f3171c
Obligedwalnut <benerogic@gmail.com>
2023-08-23 12:26:29 +0900

    fix : group api 수정(participant 폼데이터화)

--- src/controllers/group.controller.js
+++ src/controllers/group.controller.js
@@ -10,8 +10,7 @@ class GroupController {
       const { userId } = res.locals.user;
       const thumbnailUrl = await uploadImageToCloudinary(req.file.path);      
       const { groupName, place, participant, startDate, endDate } = req.body;
-      const participants = JSON.parse(participant)
-      console.log(participants)      
+      const participants = JSON.parse(participant)          
       const participantPlusUserId = participants.concat(JSON.stringify(userId));
       const createGroupData = await this.groupService.createGroup(
         userId,
