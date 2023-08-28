c1908df0328869ef48f1d87db5ae6b446fcb39e2
mjmcode <mjm7542>
2023-08-28 17:08:20 +0900

    fix : 인증 과정 수정

--- src/routes/signed.route.js
+++ src/routes/signed.route.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../utils/authMiddleware");
 const asyncHandler = require("../utils/asyncHandler");
+const generateSignedUploadParams = require("../utils/signedUpload")
 
 router.get('/', authMiddleware, asyncHandler(async (req, res) => {
     const { signature, timestamp } = await generateSignedUploadParams();
+    console.log(signature, timestamp)
     res.status(200).json({
         success: true,
         signature,
