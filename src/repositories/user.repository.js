f7bc61f9b2d1d529a90fe54b9e4226fd9c19962b
Obligedwalnut <benerogic@gmail.com>
2023-09-06 11:56:10 +0900

    fix : nickname 검색 부분일치 수정

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -6,7 +6,7 @@ class UserRepository {
   findByNickname = async (nickname, userId) => {
     const findByNicknameData = await Users.findAll({
       where: {
-        nickname: nickname,
+        nickname: { [Op.like]: `%${nickname}%` },
         userId: { [Op.ne]: userId },
       },
       attributes: ["userId", "loginId", "nickname", "profileUrl", "kakaoId"],
@@ -18,7 +18,14 @@ class UserRepository {
   userInfoByUserId = async (userId) => {
     const userInfoData = await Users.findOne({
       where: { userId: userId },
-      attributes: ["userId", "loginId", "nickname", "profileUrl", "providerType", "kakaoId"],
+      attributes: [
+        "userId",
+        "loginId",
+        "nickname",
+        "profileUrl",
+        "providerType",
+        "kakaoId",
+      ],
     });
     return userInfoData;
   };
