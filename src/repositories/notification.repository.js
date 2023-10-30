ae84b01822b1c4968552e95e8854902a2459653d
mjmcode <mjm7542>
2023-10-30 21:03:36 +0900

    fix : 알림 레포가 그룹으로 처리 되어있어서 수정

--- src/repositories/notification.repository.js
+++ src/repositories/notification.repository.js
@@ -1,64 +1,33 @@
-const { Groups, Participants, } = require("../models");
-const { Op } = require('sequelize');
+const { Notifications } = require("../models");
 
-class NotificationService {
-  creatNotification = async (userId, groupId, thumbnailUrl, message, status) => {
-    const creatNotificationData = await Groups.create({
-      userId,
-      groupId,
-      thumbnailUrl,
-      message,
-      status
-    });
-    return creatNotificationData;
-  };
-  getGroupIds = async (userId) => {
-    const getGroupIdData = await Participants.findAll({
-      where: { userId },
-      attributes: ["groupId"]
-    });
-    const groupIds = getGroupIdData.map((id) => {
-      const Id = id.groupId;
-      return Id;
-    })
-    return groupIds;
-  };
-
-
-  getNotification = async (userId, groupIds) => {
-    const getGroupData = await Groups.findAll({
-      where: {
-        groupId: {
-          [Op.or]: groupIds,
-        },
-        userId: {
-          [Op.ne]: userId
-        }
-      },
-      attributes: ['groupId', 'thumbnailUrl', 'groupName'],
-      group: ['groupId'],
-      raw: true,
-      order: [[Participants, 'createdAt', 'DESC']],
-      include: [
-        {
-          model: Participants,
-          attributes: ["participantid", "status", 'createdAt'],
-        },
-      ],
-    })
-    return getGroupData;
-  }
-  updataNotification = async (userId, participantid) => {
-    const deleteCommentData = await Participants.update(
-      { status: true },
-      {
-        where: {
-          userId,
-          participantid
-        }
-      });
-    return deleteCommentData;
-  };
+class NotificationRepository {
+    creatNotification = async (userId, groupId, thumbnailUrl, message, status) => {
+        const creatNotificationData = await Notifications.create({
+            userId,
+            groupId,
+            thumbnailUrl,
+            message,
+            status
+        });
+        return creatNotificationData;
+    };
+    getNotificationData = async (userId) => {
+        const updateCommentData = await Notifications.findAll(
+            { where: { userId } }
+        );
+        return updateCommentData;
+    };
+    updataNotification = async (userId, notificationId) => {
+        const deleteCommentData = await Notifications.update(
+            { status: 'checked' },
+            {
+                where: {
+                    userId,
+                    notificationId
+                }
+            });
+        return deleteCommentData;
+    };
 }
 
-module.exports = NotificationService;
+module.exports = NotificationRepository;
